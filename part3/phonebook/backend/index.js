d52db2fbe521335b86c38f33dccf95efbf79368d
MPHelsinkiUni <dat.t.vo@helsinki.fi>
2026-06-01 00:05:28 +0300

    3.16. done

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,52 +1,41 @@
+
+
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-morgan.token('inner_content', function (req, res) { return JSON.stringify(req.body)})
+const Person = require('./models/person')
 
-
-app.use(express.json())
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    tokens.inner_content(req, res)
-].join(' ')
-}))
+// Deal with console outputs
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
+// Deal with handling functions
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
+app.use(unknownEndpoint)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
+
+
+// App proper
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -61,39 +50,34 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+    Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }    
+    })
+    .catch(error => {next(error)})
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/persons', (request, response) => {
     const contact = request.body
-
-    const nameExists = persons.find((person) => person.name.toLowerCase() === contact.name.toLowerCase()
-    )
-        if (nameExists) {
-            return response.status(400).json({ 
-            error: 'name must be unique' 
-            })
-        }
 
   if (!contact.name) {
     return response.status(400).json({ 
@@ -108,17 +92,14 @@ app.post('/api/persons', (request, response) => {
   }
 
   const person = {
-    id: generateId(),
     name: contact.name,
     number: contact.number,
   }
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedContact => {response.json(savedContact)})
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
