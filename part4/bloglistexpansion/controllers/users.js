aef346503dc1f511bd03cc6a7cf3133745757763
MPHelsinkiUni <dat.t.vo@helsinki.fi>
2026-06-16 19:58:59 +0300

    Up to 4.19. done

--- part4/bloglistexpansion/controllers/users.js
+++ part4/bloglistexpansion/controllers/users.js
@@ -3,7 +3,14 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+  console.log(request.body)
+  const username = request.body.username
+  const name = request.body.name
+  const password = request.body.password
+
+  if (password.length < 3) {
+    return response.status(400).json({ error: 'User validation failed: password: Path `password` is shorter than the minimum allowed length (3)' })
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
@@ -14,9 +21,6 @@ usersRouter.post('/', async (request, response) => {
     passwordHash,
   })
 
-  if (password.length < 3) {
-    return response.status(400).json({ error: 'User validation failed: password: Path `password` is shorter than the minimum allowed length (3)' })
-  }
 
   const savedUser = await user.save()
 
